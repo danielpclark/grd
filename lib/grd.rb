ed1cd93846c1f86e098526711ffed4815e46da8b
danielpclark <webmaster@6ftdan.com>
2014-05-21 09:40:11 -0400

    Moduled.

--- lib/grd.rb
+++ lib/grd.rb
@@ -1,1 +1,14 @@
 require 'grd/version'
+require 'net/http'
+
+module GRD
+	def self.getredirect(in_url)
+		val = in_url.dup
+		val.prepend('http://') unless !!val['http://']
+		uri = URI.parse val
+		req = Net::HTTP::Get.new(uri.request_uri)
+		puts Net::HTTP.new(uri.host).start { |server|
+			server.request(req)
+		}["location"]
+	end
+end
--- lib/grd/version.rb
+++ lib/grd/version.rb
@@ -1,3 +1,3 @@
 module GRD
-	VERSION = "0.0.1"
+	VERSION = "0.0.2"
 end
